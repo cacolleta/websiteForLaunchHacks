fdbc93d1b70b95905f1a3e18a8cf39022555f074
adrianMoyaMartin <125206876+adrianMoyaMartin@users.noreply.github.com>
2023-07-22 21:04:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require("express");
 let app = express();
 const efu = require("express-fileupload");
 const sqlite = require("sqlite3").verbose();
+const fs = require("fs");
+
+let PORT = 3000;
 
 app.use(efu());
 app.use(express.static("public"));
 
-app.listen(3000, () => {
-  console.log(`app is listening on http://localhost:3000`);
+app.listen(PORT, () => {
+  console.log(`app is listening on http://localhost:${PORT}`);
 });

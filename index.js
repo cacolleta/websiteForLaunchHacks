b761796f7f7d0ea7421eb4204d19afaad60ce619
adrianMoyaMartin <125206876+adrianMoyaMartin@users.noreply.github.com>
2023-07-22 21:27:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { log } = require("console");
 const express = require("express");
 let app = express();
 const efu = require("express-fileupload");
@@ -8,6 +9,15 @@ let PORT = 3000;
 
 app.use(efu());
 app.use(express.static("public"));
+
+app.get("/getVideo", (req, res) => {
+  const video_Id = req.headers.video_id;
+  if (!video_Id) {
+    res.sendStatus(404);
+  } else {
+    res.send("Success!");
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`app is listening on http://localhost:${PORT}`);
